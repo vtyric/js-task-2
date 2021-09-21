b259d9674e28ece9cca62ab9ea2bcaa49648037b
Nikita Zernyukov <Nikita.Zerniukov@urfu.me>
2021-09-21 17:36:36 +0500

    Test fix

--- topic-2/task-2/specs/task-2.spec.ts
+++ topic-2/task-2/specs/task-2.spec.ts
@@ -4,9 +4,12 @@ test('Method works fine', () => {
     expect(arrayStripped([1, 2, 3, 4], 1)).toEqual([ [ 1 ], [ 2 ], [ 3 ], [ 4 ] ])
     expect(arrayStripped(['a', 'b', 'c', 'd'], 3)).toEqual([ [ 'a', 'b', 'c' ], [ 'd' ] ]);
     expect(arrayStripped([], 2)).toEqual([ ]);
+    expect(arrayStripped(undefined, 5)).toEqual([]);
+    expect(arrayStripped(null, null)).toThrow();
+    expect(arrayStripped([1, 5, 7], null)).toThrow();
 });
 
 test('Method gets incorrect data', () => {
     expect(arrayStripped('text', 5)).toEqual([]);
-    expect(arrayStripped(5, 'text')).toEqual([]);
+    expect(arrayStripped(5, 'text')).toThrow();
 })
