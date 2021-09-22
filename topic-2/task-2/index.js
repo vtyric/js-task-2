6ef023f6f870992d5c56abe96f4c0e9849816cde
makar <misha.torno@gmail.com>
2021-09-22 13:42:47 +0500

    решил вторую задачу.

--- topic-2/task-2/index.js
+++ topic-2/task-2/index.js
@@ -3,18 +3,30 @@
  * Допишите функцию, разбивающую массив на группы по размеру
  * Если массив не может быть разделен равномерно, последней частью
  * будут оставшиеся элементы. Порядок элементов внутри группы важен.
- * 
+ *
  * arrayStripped(['a', 'b', 'c', 'd'], 2)) => [['a', 'b'], ['c', 'd']]
  * arrayStripped([1, 'b', 5, 0, 'c'], 2)) => [[1, 'b'], [5, 0], ['c']]
- * 
+ *
  * Если разбивать на группы нечего, выведите пустой массив.
- * 
+ *
  * @param {*} array массив на вход
  * @param {*} size из скольких элементов состоит группа
  * @returns массив разбитый на группы
 */
 const arrayStripped = (array, size) => {
+    if (array === null || typeof (size) !== "number") {
+        throw new Error();
+    }
+    if (array === undefined || typeof (array) === array) {
+        return [];
+    }
+    let res = [];
 
+    for (let i = 0; i < array.length; i += size) {
+        res.push(array.slice(i, i + size));
+    }
+
+    return res;
 }
 
 
